a9388ab9135e00a3a95968c8f72f26bb43af5994
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-12-17 22:28:03 +0100

    ref: function names corrected

--- Sources/InstallSoftware.ps1
+++ Sources/InstallSoftware.ps1
@@ -3,8 +3,8 @@ Import-Module -Name .\Utils.ps1
 # Package manager: https://community.chocolatey.org/
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-InstaChocolateyPackages
+Install-ChocolateyPackages
 Install-Module posh-git -Scope CurrentUser -Force
 Import-Module posh-git
 Add-PoshGitToProfile -AllHosts
-InstallVSCodeExtensions
+Install-VSCodeExtensions
